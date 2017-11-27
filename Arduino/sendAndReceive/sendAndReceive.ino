a18902a3377eeec5c98a961df98c7adbd2a3811f
Kars Alfrink <kars@leapfrog.nl>
2017-11-27 10:36:17 +0100

    Move wifi functions to separate file

--- Arduino/sendAndReceive/sendAndReceive.ino
+++ Arduino/sendAndReceive/sendAndReceive.ino
@@ -5,6 +5,7 @@
 #include <OSCBoards.h>
 
 #include "config.h" // copy config_sample.h and rename it
+#include "wifiHelpers.h" // helper functions for wifi
 
 char ssid[] = MYSSID;            // your network SSID (name)
 char pass[] = MYPASS;  // your network password
@@ -129,62 +130,5 @@ void setLed(int b) {
   analogWrite(led, b);
   Serial.print("Set LED brightness to: ");
   Serial.println(b);
-  Serial.println();
-}
-
-void printWiFiData() {
-  // Print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP address: ");
-  Serial.println(ip);
-
-  // Print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  Serial.print(mac[5], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.println(mac[0], HEX);
-  Serial.println();
-}
-
-void printCurrentNet() {
-  // Print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // Print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5], HEX);
-  Serial.print(":");
-  Serial.print(bssid[4], HEX);
-  Serial.print(":");
-  Serial.print(bssid[3], HEX);
-  Serial.print(":");
-  Serial.print(bssid[2], HEX);
-  Serial.print(":");
-  Serial.print(bssid[1], HEX);
-  Serial.print(":");
-  Serial.println(bssid[0], HEX);
-
-  // Print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("Signal strength (RSSI): ");
-  Serial.println(rssi);
-
-  // Print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type: ");
-  Serial.println(encryption, HEX);
   Serial.println();
 }
